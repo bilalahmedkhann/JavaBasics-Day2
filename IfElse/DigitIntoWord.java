a3910e6dcf01d7f30f943d229d3c12ea6d6bc230
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-08 19:47:43 +0530

    [ADDED]:Java Basic If Else Statement Problems

---
+++ IfElse/DigitIntoWord.java
@@ -0,0 +1,51 @@
+/*          Read a single Digit Number and write in word         */
+
+package com.IfElse;
+
+import java.util.Scanner;
+
+public class DigitIntoWord {
+    public static void main(String[] args) {
+        System.out.println("Enter any number between 0 to 9: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num < 10) {
+            selectAnyNumber(num);
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
+
+    private static void selectAnyNumber(int num) {
+        if (num == 0) {
+            System.out.println(num + " in digit is zero.");
+        } else if (num == 1) {
+            System.out.println(num + " in digit is one.");
+
+        } else if (num == 2) {
+            System.out.println(num + " in digit is two.");
+
+        } else if (num == 3) {
+            System.out.println(num + " in digit is three.");
+
+        } else if (num == 4) {
+            System.out.println(num + " in digit is four.");
+
+        } else if (num == 5) {
+            System.out.println(num + " in digit is five.");
+
+        } else if (num == 6) {
+            System.out.println(num + " in digit is six.");
+
+        } else if (num == 7) {
+            System.out.println(num + " in digit is seven.");
+
+        } else if (num == 8) {
+            System.out.println(num + " in digit is eight.");
+
+        } else if (num == 9) {
+            System.out.println(num + " in digit is nine.");
+
+        }
+    }
+}
---
+++ IfElse/ReadANumber.java
@@ -0,0 +1,30 @@
+package com.IfElse;
+
+import java.util.Scanner;
+
+public class ReadANumber {
+    public static void main(String[] args) {
+        System.out.println("Choose any number between 0 to 10000: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num > 0 && num < 10000) {
+            numberToUnits(num);
+        } else {
+            System.out.println("Invalid input");
+        }
+
+
+    }
+
+    private static void numberToUnits(int num) {
+        if (num > 0 && num < 10) {
+            System.out.println("Number is " + num + ", its unit is Ones");
+        } else if (num > 9 && num < 100) {
+            System.out.println("Number is " + num + ", its unit is Tens");
+        } else if (num > 99 && num < 1000) {
+            System.out.println("Number is " + num + ", its unit is Hundred");
+        } else if (num > 999 && num < 10000) {
+            System.out.println("Number is " + num + ", its unit is Thousand");
+        }
+    }
+}
