7c91f1784339768e46d86c31ba782ef093758cc2
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-09 10:14:11 +0530

    [ADDED]:Program to find Min and Max

--- OperatorPrograms/Operators.java
+++ OperatorPrograms/Operators.java
@@ -3,28 +3,32 @@ package com.OperatorPrograms;
 import java.util.Scanner;
 
 public class Operators {
-    int a, b, c;
-    int result1, result2, result3, result4;
+    static String forMaxRes = null;
+    static String forMinRes = null;
 
-    String forMaxRes = null;
-    String forMinRes = null;
+    public static void main(String[] args) {
+        evaluateAndFindMinMax();
 
-    public void evaluateAndFindMinMax()  {
+
+    }
+
+
+    public static void evaluateAndFindMinMax() {
         System.out.println("Enter any three numbers: ");
         Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-        result1 = a + b * c;
-        result2 = c + a / b;
-        result3 = a % b + c;
-        result4 = a * b + c;
-        System.out.println("Maximim Result is " + max(result1, result2, result3, result4) + " for " + forMaxRes);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int result1 = a + b * c;
+        int result2 = c + a / b;
+        int result3 = a % b + c;
+        int result4 = a * b + c;
+        System.out.println("Maximum Result is " + max(result1, result2, result3, result4) + " for " + forMaxRes);
         System.out.println("Minimum result is " + min(result1, result2, result3, result4) + " for " + forMinRes);
 
     }
 
-    public int max(int result1, int result2, int result3, int result4) {
+    public static int max(int result1, int result2, int result3, int result4) {
         int max = result1;
         forMaxRes = "a + b * c";
         if (result2 > max) {
@@ -42,7 +46,7 @@ public class Operators {
         return max;
     }
 
-    public int min(int result1, int result2, int result3, int result4) {
+    public static int min(int result1, int result2, int result3, int result4) {
         int min = result1;
         forMinRes = "a + b * c";
         if (result2 < min) {
